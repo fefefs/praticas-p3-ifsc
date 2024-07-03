edc652bd108ec5b1d1f9dd64cfd6dd671e9f8486
Fernanda Cardoso Serafim <fernanda.cs20@aluno.ifsc.edu.br>
2024-07-03 15:35:49 -0300

    Lista-revisão

---
+++ Lista-revisão/src/ifsc/Animal.java
@@ -0,0 +1,25 @@
+package ifsc;
+
+public abstract class Animal {
+	
+	private  String classe;
+	public String getClasse() {
+		return classe;
+	}
+
+	public void setClasse(String classe) {
+		this.classe = classe;
+	}
+	//////////////////////////////////////////////
+	private String nomeCientifico;
+	public String getNomeCientifico() {
+		return nomeCientifico;
+	}
+
+	public void setNomeCientifico(String nomeCientifico) {
+		this.nomeCientifico = nomeCientifico;
+	}
+
+	
+
+}
---
+++ Lista-revisão/src/ifsc/Tutor.java
@@ -0,0 +1,28 @@
+package ifsc;
+
+public class Tutor {
+	
+	private String nomeCompleto;
+
+	public String getNomeCompleto() {
+		return nomeCompleto;
+	}
+
+	public void setNomeCompleto(String nomeCompleto) {
+		this.nomeCompleto = nomeCompleto;
+	}
+	
+	////////////////////////////////////////////////////////
+	
+	private Gato gato;
+	public Gato getGato() {
+		return gato;
+	}
+
+	public void setGato(Gato gato) {
+		this.gato = gato;
+	}
+
+	
+
+}
